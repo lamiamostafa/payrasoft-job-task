b9a63d28ce452fc070358a15850d7861af95653e
lamiamostafa <lamiamostafa791@gmail.com>
2022-08-21 01:04:10 +0600

    header-part is created

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div class="left-side">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <div>
+                        <h2></h2>
+                        <h2 >Dhaka City College</h2>
+                    </div>
+
+                </div>
+            </div>
+            <div class="right-side"></div>
+
+        </div>
+    );
+};
+
+export default Header;
